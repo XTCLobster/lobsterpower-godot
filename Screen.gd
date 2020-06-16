11c004ae4815677b46ba253cb199fffce816327d
Christian Tietze <christian.tietze@gmail.com>
2020-06-16 23:33:06 +0200

    fit background programmatically

--- Screen.gd
+++ Screen.gd
@@ -5,5 +5,9 @@ var screen_size: Vector2
 
 func _ready() -> void:
     screen_size = get_viewport_rect().size
+
+    $Background.set_position(Vector2(0, 0))
+    $Background.set_size(screen_size)
+
     lobster.position.x = screen_size.x / 2
     lobster.position.y = screen_size.y / 2
