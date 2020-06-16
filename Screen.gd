434eb87b0ba9af58548ccae38b1a01a7325c9795
Christian Tietze <christian.tietze@gmail.com>
2020-06-16 23:40:01 +0200

    refactor game launch

--- Screen.gd
+++ Screen.gd
@@ -1,16 +1,13 @@
 extends Node2D
 
-onready var lobster: Area2D = $Lobster
-var screen_size: Vector2
-
 func _ready() -> void:
-    screen_size = get_viewport_rect().size
+    var screen_size = get_viewport_rect().size
 
     $Background.set_position(Vector2(0, 0))
     $Background.set_size(screen_size)
 
-    lobster.position.x = screen_size.x / 2
-    lobster.position.y = screen_size.y / 2
+    $Lobster.position.x = screen_size.x / 2
+    $Lobster.position.y = screen_size.y / 2
 
     $StarvationTimer.connect("timeout", self, "_on_StarvationTimer_timeout")
 
