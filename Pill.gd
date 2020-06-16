db14ed919fb7fa530ca90357ededd0d5435e9641
Christian Tietze <christian.tietze@gmail.com>
2020-06-17 00:15:44 +0200

    spawn pills

---
+++ Pill.gd
@@ -0,0 +1,3 @@
+extends Area2D
+
+export var value: int = 666
--- Screen.gd
+++ Screen.gd
@@ -1,7 +1,12 @@
 extends Node2D
 
+export (PackedScene) var Pill
+var screen_size: Vector2
+
 func _ready() -> void:
-    var screen_size = get_viewport_rect().size
+    randomize()
+
+    screen_size = get_viewport_rect().size
 
     $Background.set_position(Vector2(0, 0))
     $Background.set_size(screen_size)
@@ -9,7 +14,17 @@ func _ready() -> void:
     $Lobster.position.x = screen_size.x / 2
     $Lobster.position.y = screen_size.y / 2
 
+    $PillSpawnTimer.connect("timeout", self, "_on_PillSpawnTimer_timeout")
     $StarvationTimer.connect("timeout", self, "_on_StarvationTimer_timeout")
 
-func _on_StarvationTimer_timeout():
+func _on_PillSpawnTimer_timeout() -> void:
+    var pill = Pill.instance()
+    pill.position = Vector2(
+        rand_range(0, screen_size.x),
+        rand_range(0, screen_size.y)
+    )
+    pill.visible = true
+    add_child(pill)
+
+func _on_StarvationTimer_timeout() -> void:
     $Stats.increment_score(-1)
