46f55f0a9b76d0a7c3ff836d5a0cc7ff2c8203f6
Christian Tietze <christian.tietze@gmail.com>
2020-06-16 23:29:08 +0200

    move lobster with the keyboard

---
+++ Lobster.gd
@@ -0,0 +1,31 @@
+extends Area2D
+
+export var speed: float = 200
+var screen_size: Vector2
+
+func _ready() -> void:
+    screen_size = get_viewport_rect().size
+
+func _process(delta: float) -> void:
+    var velocity = velocity()
+    position += velocity * delta
+    position.x = clamp(position.x, 0, screen_size.x)
+    position.y = clamp(position.y, 0, screen_size.y)
+
+func velocity() -> Vector2:
+    # Compute the movement vector based on input
+    var velocity = Vector2()
+    if Input.is_action_pressed("ui_right"):
+        velocity.x += 1
+    if Input.is_action_pressed("ui_left"):
+        velocity.x -= 1
+    if Input.is_action_pressed("ui_down"):
+        velocity.y += 1
+    if Input.is_action_pressed("ui_up"):
+        velocity.y -= 1
+
+    # Apply player speed in input direction
+    if velocity.length() > 0:
+        velocity = velocity.normalized() * speed
+
+    return velocity
--- Screen.gd
+++ Screen.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
-onready var lobster = $Lobster
+onready var lobster: Area2D = $Lobster
+var screen_size: Vector2
 
 func _ready() -> void:
-    pass
+    screen_size = get_viewport_rect().size
+    lobster.position.x = screen_size.x / 2
+    lobster.position.y = screen_size.y / 2
