412423df66bcd9975d51a4df4f5c109d73e65d93
Christian Tietze <christian.tietze@gmail.com>
2020-06-18 14:32:03 +0200

    change Lobster to a physics body

--- Lobster.gd
+++ Lobster.gd
@@ -1,29 +1,26 @@
-extends Area2D
+extends KinematicBody2D
 
 export var base_speed: float = 200
 var speed: float = base_speed
+var velocity: Vector2 = Vector2()
 onready var screen_size: Vector2 = get_viewport_rect().size
 
+func _physics_process(delta: float) -> void:
+    if velocity.length() > 0:
+        velocity = velocity.normalized() * speed
+        move_and_collide(velocity * delta)
+
 func _process(delta: float) -> void:
-    var velocity = velocity()
-    position += velocity * delta
     position.x = clamp(position.x, 0, screen_size.x)
     position.y = clamp(position.y, 0, screen_size.y)
 
-func velocity() -> Vector2:
-    # Compute the movement vector based on input
-    var velocity = Vector2()
+func _input(event: InputEvent) -> void:
+    self.velocity = Vector2()
     if Input.is_action_pressed("ui_right"):
-        velocity.x += 1
+        self.velocity.x = 1
     if Input.is_action_pressed("ui_left"):
-        velocity.x -= 1
+        self.velocity.x = -1
     if Input.is_action_pressed("ui_down"):
-        velocity.y += 1
+        self.velocity.y = 1
     if Input.is_action_pressed("ui_up"):
-        velocity.y -= 1
-
-    # Apply player speed in input direction
-    if velocity.length() > 0:
-        velocity = velocity.normalized() * speed
-
-    return velocity
+        self.velocity.y = -1
